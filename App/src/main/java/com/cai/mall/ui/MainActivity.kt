74eb97371da4ae8bb807cc73b944ee26bce9bc22
ivan <zhengmc123456@gmail.com>
2018-03-14 23:39:12 +0800

    添加UserCenter module

---
+++ App/src/main/java/com/cai/mall/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.cai.mall.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.cai.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+    }
+
+
+}
---
+++ UserCenter/src/main/java/com/cai/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.cai.user.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.cai.user.R
+
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+
+}
